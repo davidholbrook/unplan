ecff7fc1dc10153d02057864577aca21d52535da
David Holbrook <david@davidholbrook.info>
2018-11-09 17:18:36 -0600

    added routing via react-router
    Signed-off-by: David Holbrook <david@davidholbrook.info>

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Helmet from "react-helmet";
+
+const Index = () => {
+  return (
+    <div>
+      <Helmet title="Hello" />
+      Hello there 🤮
+    </div>
+  );
+};
+
+export default Index;

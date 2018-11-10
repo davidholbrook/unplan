0e7e3902cbec0f922409e2980e53aed2473e2fda
David Holbrook <david@davidholbrook.info>
2018-11-10 16:15:08 -0600

    added webdash support
    Signed-off-by: David Holbrook <david@davidholbrook.info>

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "globDirectory": "./build/",
+    "globPatterns": [
+        "**/*.{html,js,css,png,jpg,svg}"
+    ],
+    "globIgnores": [],
+    "swDest": "./build/service-worker.js"
+};

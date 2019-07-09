59ba015771905af723a15e7fbcc45ade9cfef0fa
David Holbrook <david@davidholbrook.info>
2019-07-08 20:01:58 -0500

    Wrote copy for the goals page.
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- src/pages/goals.js
+++ src/pages/goals.js
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import GoalsIcon from "../components/icons/goals";
 
 const Goals = () => {
   return (
     <div>
-      <h1>Something goes here...</h1>
+      <h1>
+        <GoalsIcon iconFill="#fd7014" />
+        <Title>Your Goals</Title>
+      </h1>
+      <p>
+        Goals are important to keep your life in line. Below is your list of
+        goals that you are currently tracking.
+      </p>
     </div>
   );
 };
 
 export default Goals;
+
+const Title = styled.strong`
+  text-transform: uppercase;
+  color: #fd7014;
+`;

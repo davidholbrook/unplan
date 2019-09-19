ce32794fd5535f372766dd7e6a8fd34d8bfadf36
David Holbrook <david@davidholbrook.info>
2019-09-18 20:51:01 -0500

    updated packages and removed unneeded files
    Signed-off-by: David Holbrook <david@davidholbrook.info>

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "globDirectory": "./build/",
-    "globPatterns": [
-        "**/*.{html,js,css,png,jpg,svg}"
-    ],
-    "globIgnores": [],
-    "swDest": "./build/service-worker.js"
-};
